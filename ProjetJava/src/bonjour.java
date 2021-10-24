9f4bbcdbcc2b05e2f5b423b5867c574db7bf51f7
PAMMANE YOUNES <pammane@gmail.com>
2021-10-24 22:42:46 +0100

    First programme Java

---
+++ ProjetJava/src/bonjour.java
@@ -0,0 +1,17 @@
+public class bonjour {
+
+	public static void main(String[] args) {
+		int n=6; //Declaration et initialisation
+		double x= 5.9;
+		char car='a';
+		boolean marcher=true;
+		String mot="Salut";
+		System.out.println("Bonjour");
+		System.out.println(n);
+		System.out.println(x);
+		System.out.println(car);
+		System.out.println(marcher);
+		System.out.println(mot);		
+	}
+
+}
